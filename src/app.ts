96f845e36fe65c9aebf38547905abda06b70846d
kauan-carvalho-code <kauancarvalho987@gmail.com>
2024-01-26 01:49:40 -0300

    feat: swagger has been added

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,13 @@ import cors from 'cors'
 
 import cookieParser from 'cookie-parser'
 
+import swaggerUi from 'swagger-ui-express'
+
 import { authRouter } from './modules/auth/routes/auth-routes'
 import { userRouter } from './modules/users/routes/user-routes'
 import { appointmentRouter } from './modules/appointments/routes/appointment-routes'
+
+import { swaggerSpec } from './config/swagger'
 
 const app = express()
 
@@ -25,6 +29,7 @@ app.use(
 
 app.use(cookieParser())
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }))
 app.use('/auth', authRouter)
 app.use('/users', userRouter)
 app.use('/appointments', appointmentRouter)
